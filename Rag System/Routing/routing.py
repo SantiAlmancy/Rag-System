076730dc0f9576860563e751fd5d75f544a84f4b
SantiAlmancy <santialmancy@gmail.com>
2024-11-04 03:00:17 -0400

    develop - Quick Fix

--- Rag System/Routing/routing.py
+++ Rag System/Routing/routing.py
@@ -5,7 +5,7 @@ import pickle
 def getEmbedding(embedModel, text):
     return embedModel.embed_query(text)
 
-def isSpecificModelSimilar(embedModel, query, topics, threshold=0.80, fuzzThreshold=75):
+def isSpecificModelSimilar(embedModel, query, topics, threshold=0.70, fuzzThreshold=70):
     # Generate embedding for the query once
     queryEmbedding = getEmbedding(embedModel, query)
 
